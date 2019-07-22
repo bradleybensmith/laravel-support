ecd60e60728cfdd83e4bb275724de47c7dd3e0b0
Bradley B. Smith <bradley.smith@its-florida.net>
2019-07-22 12:06:42 -0400

    Initial release of functions

---
+++ src/Providers/SupportServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Laravel\Support\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class SupportServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->bootBladeDirectives();
+        $this->bootMacros();
+    }
+
+    private function bootBladeDirectives()
+    {
+        // Add @optional for Complex Yielding
+        Blade::directive('optional', static function($expression) {
+            return "<?php if(trim(\$__env->yieldContent({$expression}))): ?>";
+        });
+
+        // Add @endoptional for Complex Yielding
+        Blade::directive('endoptional', static function($expression) {
+            return "<?php endif; ?>";
+        });
+    }
+
+    private function bootMacros()
+    {
+        // Add Route::if('route.name', 'is-active') for simpler styling
+        Route::macro('if', function(...$patterns) {
+            $true = array_pop($patterns);
+            return Route::is(...$patterns) ? $true : '';
+        });
+    }
+}
